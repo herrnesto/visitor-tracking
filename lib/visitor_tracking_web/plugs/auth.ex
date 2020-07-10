cff0a6c332cc2b1ec926d5702b8a0faeba2509a2
George Mantzouranis <gemantzu@gmail.com>
2020-07-10 23:31:50 +0300

    auth plug

---
+++ lib/visitor_tracking_web/plugs/auth.ex
@@ -0,0 +1,18 @@
+defmodule VisitorTrackingWeb.Plugs.Auth do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && VisitorTracking.Accounts.get_user(:user_id)
+    put_session(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+end
