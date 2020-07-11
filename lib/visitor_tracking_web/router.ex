89c78238437fd96aa6d9db5bd4ff236aea4518a4
Simon Kistler <simon@kistler.email>
2020-07-11 22:32:12 +0200

    dev tools

--- lib/visitor_tracking_web/router.ex
+++ lib/visitor_tracking_web/router.ex
@@ -32,7 +32,7 @@ defmodule VisitorTrackingWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
+  if Application.get_env(:visitor_tracking, :developer_tools) == "true" do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
