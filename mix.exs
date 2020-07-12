8dfd6d9766e9e85e158a97969ae2ae28a1bea81a
Simon Kistler <simon@kistler.email>
2020-07-12 17:45:49 +0200

    added httpoison deps

--- mix.exs
+++ mix.exs
@@ -55,11 +55,11 @@ defmodule VisitorTracking.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:bamboo, "~> 1.5"},
       {:pbkdf2_elixir, "~> 1.0"},
-      # Dev / Test specific deps
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:excoveralls, "~> 0.13.0", only: :test},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_machina, "~> 2.4", only: :test}
+      {:ex_machina, "~> 2.4", only: :test},
+      {:httpoison, "~> 1.6"}
     ]
   end
 
