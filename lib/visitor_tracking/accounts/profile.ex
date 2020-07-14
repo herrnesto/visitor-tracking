23fadbccc383a555c6679ad2c0af7e2df036be45
George Mantzouranis <gemantzu@gmail.com>
2020-07-15 01:02:33 +0300

    login / reg / verify working

--- lib/visitor_tracking/accounts/profile.ex
+++ lib/visitor_tracking/accounts/profile.ex
@@ -24,4 +24,10 @@ defmodule VisitorTracking.Accounts.Profile do
       message: "invalid mobile number, must be of format +XXXXXXXXX"
     )
   end
+
+  def phone_verification_changeset(profile, attrs) do
+    profile
+    |> cast(attrs, [:phone_verified])
+    |> validate_required([:phone_verified])
+  end
 end
