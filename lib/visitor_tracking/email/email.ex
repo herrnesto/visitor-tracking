bdd3ee471b0cfa5cbfc43cf8659d440a34034c06
Simon Kistler <simon@kistler.email>
2020-07-16 20:53:19 +0200

    Added url to verify email

--- lib/visitor_tracking/email/email.ex
+++ lib/visitor_tracking/email/email.ex
@@ -7,8 +7,8 @@ defmodule VisitorTracking.Email do
   def verification_email(email, token) do
     base_email()
     |> to(email)
-    |> subject("Bestätige deine Email")
-    |> assign(:args, %{email: email, token: token})
+    |> subject("Bestätige deine E-Mail-Adresse")
+    |> assign(:args, %{email: email, url: get_verification_url(token)})
     |> render(:verification)
   end
 
@@ -19,5 +19,9 @@ defmodule VisitorTracking.Email do
     # This will use the "email.html.eex" file as a layout when rendering html emails.
     # Plain text emails will not use a layout unless you use `put_text_layout`
     |> put_html_layout({VisitorTrackingWeb.LayoutView, "email.html"})
+  end
+
+  def get_verification_url(token) do
+    VisitorTrackingWeb.Endpoint.url <>"/v/#{token}"
   end
 end
