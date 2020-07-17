4041159bcd7e7f84d7a319fb04659319790eee56
Simon Kistler <simon@kistler.email>
2020-07-17 20:37:09 +0200

    redesigned buttons

--- assets/js/components/ConfirmationButton/ConfirmationButton.js
+++ assets/js/components/ConfirmationButton/ConfirmationButton.js
@@ -3,23 +3,30 @@ import { useAsync } from 'react-async'
 import { confirmVisitor } from '../../api'
 
 const ConfirmationButton = ({ action, state, dispatch }) => {
-  let text, style, apiCall, handleClick
+  let text, style, icon, apiCall, handleClick
   const processVisitor = () => dispatch({ type: 'processed' })
 
   if (action === "confirm") {
-    text = "Confirm visitor"
+    text = "Gast bestätigen"
     style = "is-success"
+    icon = "fa-check"
     apiCall = useAsync({ deferFn: confirmVisitor })
     handleClick = useCallback(() => apiCall.run(processVisitor, state.code, state.eventId), [])
   } else {
-    text = "Decline visitor"
+    text = "Ablehnen"
     style = "is-danger"
+    icon = "fa-ban"
     handleClick = useCallback(() => processVisitor())
   }
 
   return (
-    <button className={`button ${style}`} onClick={handleClick}>
-      {text}
+    <button className={`button is-medium ${style}`} onClick={handleClick}>
+      <span class={`icon is-small`}>
+        <i class={`fas ${icon}`}></i>
+      </span>
+      <span>
+        {text}
+      </span>
     </button>
   )
 }
