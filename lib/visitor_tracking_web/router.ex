7e291e06cc973627bade40570c6faa98a6c81bf9
Simon Kistler <simon@kistler.email>
2020-07-24 14:56:19 +0200

    basic crud functionality

--- lib/visitor_tracking_web/router.ex
+++ lib/visitor_tracking_web/router.ex
@@ -43,7 +43,7 @@ defmodule VisitorTrackingWeb.Router do
 
     get "/expecting_verification", ProfileController, :expecting_verification
     get "/v/:token", ProfileController, :verify_email
-    get "/events", EventController, :index
+    resources "/events", EventController
     get "/events/:event_id/scan", ScanController, :show
     get "/profile", ProfileController, :show
     get "/profile/qrcode", ProfileController, :show
---
+++ test/visitor_tracking_web/controllers/event_controller_test.exs
@@ -0,0 +1,88 @@
+defmodule VisitorTrackingWeb.EventControllerTest do
+  use VisitorTrackingWeb.ConnCase
+
+  alias VisitorTracking.Events
+
+  @create_attrs %{closed: true, date_end: "2010-04-17T14:00:00Z", date_start: "2010-04-17T14:00:00Z", description: "some description", name: "some name", status: "some status", venue: "some venue"}
+  @update_attrs %{closed: false, date_end: "2011-05-18T15:01:01Z", date_start: "2011-05-18T15:01:01Z", description: "some updated description", name: "some updated name", status: "some updated status", venue: "some updated venue"}
+  @invalid_attrs %{closed: nil, date_end: nil, date_start: nil, description: nil, name: nil, status: nil, venue: nil}
+
+  def fixture(:event) do
+    {:ok, event} = Events.create_event(@create_attrs)
+    event
+  end
+
+  describe "index" do
+    test "lists all events", %{conn: conn} do
+      conn = get(conn, Routes.event_path(conn, :index))
+      assert html_response(conn, 200) =~ "Listing Events"
+    end
+  end
+
+  describe "new event" do
+    test "renders form", %{conn: conn} do
+      conn = get(conn, Routes.event_path(conn, :new))
+      assert html_response(conn, 200) =~ "New Event"
+    end
+  end
+
+  describe "create event" do
+    test "redirects to show when data is valid", %{conn: conn} do
+      conn = post(conn, Routes.event_path(conn, :create), event: @create_attrs)
+
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == Routes.event_path(conn, :show, id)
+
+      conn = get(conn, Routes.event_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Show Event"
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.event_path(conn, :create), event: @invalid_attrs)
+      assert html_response(conn, 200) =~ "New Event"
+    end
+  end
+
+  describe "edit event" do
+    setup [:create_event]
+
+    test "renders form for editing chosen event", %{conn: conn, event: event} do
+      conn = get(conn, Routes.event_path(conn, :edit, event))
+      assert html_response(conn, 200) =~ "Edit Event"
+    end
+  end
+
+  describe "update event" do
+    setup [:create_event]
+
+    test "redirects when data is valid", %{conn: conn, event: event} do
+      conn = put(conn, Routes.event_path(conn, :update, event), event: @update_attrs)
+      assert redirected_to(conn) == Routes.event_path(conn, :show, event)
+
+      conn = get(conn, Routes.event_path(conn, :show, event))
+      assert html_response(conn, 200) =~ "some updated description"
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, event: event} do
+      conn = put(conn, Routes.event_path(conn, :update, event), event: @invalid_attrs)
+      assert html_response(conn, 200) =~ "Edit Event"
+    end
+  end
+
+  describe "delete event" do
+    setup [:create_event]
+
+    test "deletes chosen event", %{conn: conn, event: event} do
+      conn = delete(conn, Routes.event_path(conn, :delete, event))
+      assert redirected_to(conn) == Routes.event_path(conn, :index)
+      assert_error_sent 404, fn ->
+        get(conn, Routes.event_path(conn, :show, event))
+      end
+    end
+  end
+
+  defp create_event(_) do
+    event = fixture(:event)
+    %{event: event}
+  end
+end
