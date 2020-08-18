cd7b6ca5dc03ec6a741772e49460dbd039a7626b
Simon Kistler <simon@kistler.email>
2020-08-18 09:26:37 +0200

    enforce https

--- lib/visitor_tracking_web/plugs/WWWRedirect.ex
+++ lib/visitor_tracking_web/plugs/WWWRedirect.ex
@@ -6,23 +6,41 @@ defmodule App.Plugs.WWWRedirect do
   end
 
   def call(conn, _options) do
+    conn
+    |> redirect_https()
+    |> redirect_www()
+  end
+
+  def redirect_https(conn) do
+    case conn.scheme do
+      "http" ->
+        conn
+        |> Phoenix.Controller.redirect(external: "https://#{conn.host}")
+        |> halt()
+
+      _ ->
+        conn
+    end
+  end
+
+  def redirect_www(conn) do
     if bare_domain?(conn.host) do
       conn
       |> Phoenix.Controller.redirect(external: www_url(conn))
       |> halt
     else
-      conn # Since all plugs need to return a connection
+      conn
     end
   end
 
-  # Returns URL with www prepended for the given connection. Note this also
-  # applies to hosts that already contain "www"
   defp www_url(conn) do
     "#{conn.scheme}://www.#{conn.host}"
   end
 
-  # Returns whether the domain is bare (no www)
   defp bare_domain?(host) do
-    !Regex.match?(~r/^([A-Za-z0-9](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?(?:\.[A-Za-z0-9](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?){2,})$/i, host)
+    !Regex.match?(
+      ~r/^([A-Za-z0-9](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?(?:\.[A-Za-z0-9](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?){2,})$/i,
+      host
+    )
   end
 end
